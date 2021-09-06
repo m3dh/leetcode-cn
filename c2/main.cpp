284ff91d58f4a37728ec5d25c959e15092552b9b
Liam Yu <liamyu@live.com>
2021-09-06 15:52:59 -0700

    Tea..

--- c2/main.cpp
+++ c2/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <string>
-#include "sept21.h"
+#include "gflags/gflags.h"
+#include "inc/sept21.h"
 
-int main(void) {
+DEFINE_int32(lccn_code, 0, "A useless input code");
+
+int main(int argc, char **argv) {
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    std::cout << "Code = " << FLAGS_lccn_code << std::endl;
+
     Sept21::Solution s {};
     auto result = s.balancedStringSplit(std::string("LLRRLRLR"));
     std::cout << "Result: " << result << std::endl;
