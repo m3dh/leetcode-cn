653246aae74e80cd5852ef1f7b58aac5297e3949
Liam Yu <liamyu@live.com>
2021-09-06 21:13:18 -0700

    alc...

--- c2/inc/sept21.h
+++ c2/inc/sept21.h
@@ -1,29 +1,64 @@
+#include <queue>
 #include <string>
 
-namespace Sept21 {
+#include "alc.h"
+
+namespace sept21 {
 using namespace std;
+using namespace alc;
 
 // the solution class for programming quiz's in Sept. 2021
 class Solution {
-public:
-    int balancedStringSplit(string s) {
-        int l = 0;
-        int r = 0;
-        int cnt = 0;
-        for (auto ch : s) {
-            if (ch == 'L') {
-                ++l;
-            } else {
-                ++r;
-            }
+ public:
+  // https://leetcode-cn.com/problems/sum-of-left-leaves/
+  int sumOfLeftLeaves(TreeNode* root) {
+    int sum = 0;
+    queue<TreeNode*> level{};
+    if (root != nullptr) {
+      // root cannot be a left leaf node.
+      level.push(root);
+    }
 
-            if (l == r) {
-                ++cnt;
-            }
+    while (level.size() > 0) {
+      int size = level.size();
+      for (int i = 0; i < size; ++i) {
+        TreeNode* node = level.front();
+        level.pop();
+        if (node->left != nullptr) {
+          if (node->left->left == nullptr && node->left->right == nullptr) {
+            sum += node->left->val;
+          }
+          level.push(node->left);
         }
 
-        return cnt;
+        if (node->right != nullptr) {
+          level.push(node->right);
+        }
+      }
     }
+
+    return sum;
+  }
+
+  // https://leetcode-cn.com/problems/split-a-string-in-balanced-strings/
+  int balancedStringSplit(string s) {
+    int l = 0;
+    int r = 0;
+    int cnt = 0;
+    for (auto ch : s) {
+      if (ch == 'L') {
+        ++l;
+      } else {
+        ++r;
+      }
+
+      if (l == r) {
+        ++cnt;
+      }
+    }
+
+    return cnt;
+  }
 };
 
-}
+}  // namespace sept21
--- c2/main.cpp
+++ c2/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <string>
+
+#include "folly/json.h"
 #include "gflags/gflags.h"
 #include "inc/sept21.h"
 
 DEFINE_int32(lccn_code, 0, "A useless input code");
 
+void runSolution() {
+  sept21::Solution s{};
+  auto result = s.balancedStringSplit(std::string("LLRRLRLR"));
+  std::cout << "Result: " << folly::toPrettyJson(result) << std::endl;
+}
+
 int main(int argc, char **argv) {
-    gflags::ParseCommandLineFlags(&argc, &argv, true);
-
-    std::cout << "Code = " << FLAGS_lccn_code << std::endl;
-
-    Sept21::Solution s {};
-    auto result = s.balancedStringSplit(std::string("LLRRLRLR"));
-    std::cout << "Result: " << result << std::endl;
-
-    return 0;
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  std::cout << "Input: Code = " << FLAGS_lccn_code << std::endl;
+  std::cout << "========= Solution =========" << std::endl;
+  runSolution();
+  return 0;
 }
