8832a21c491ad4a88f7d77dd504270f5e33d6d04
Liam Yu <liamyu@live.com>
2021-09-07 12:40:33 -0700

    9/7/21 - 502

--- c2/inc/sept21.h
+++ c2/inc/sept21.h
@@ -1,7 +1,10 @@
+#include <algorithm>
+#include <iostream>
 #include <queue>
 #include <string>
 
 #include "alc.h"
+#include "glog/logging.h"
 
 namespace sept21 {
 using namespace std;
@@ -58,6 +61,60 @@ class Solution {
     }
 
     return cnt;
+  }
+
+  struct Project {
+    int profit;
+    int capital;
+    Project(int profit, int capital) : profit{profit}, capital{capital} {}
+
+    ~Project() {
+      cout << "[DTOR] PROF: " << profit << ", CAPT: " << capital << endl;
+    }
+  };
+
+  // https://leetcode-cn.com/problems/ipo/
+  int findMaximizedCapital(int k, int w, vector<int>& profits,
+                           vector<int>& capital) {
+    // find the affordable best ROI project
+    auto profitComp = [](Project& a, Project& b) {
+      return a.profit < b.profit;
+    };
+    auto profitMaxHeap =
+        priority_queue<Project, vector<Project>, decltype(profitComp)>{
+            profitComp};
+
+    auto projects = vector<Project>{};
+    projects.reserve(profits.size());
+    for (int i = 0; i < profits.size(); ++i) {
+      projects.emplace_back(profits[i], capital[i]);
+    }
+
+    auto capComp = [](Project& a, Project& b) { return a.capital < b.capital; };
+    sort(projects.begin(), projects.end(), capComp);
+
+    auto projIndex = 0;
+    while (k > 0) {
+      if (projIndex < projects.size() && projects[projIndex].capital <= w) {
+        const Project& proj = projects[projIndex];
+        profitMaxHeap.push(move(proj));
+        ++projIndex;
+      } else {
+        if (profitMaxHeap.size() == 0) {
+          break;
+        } else {
+          const Project& proj = profitMaxHeap.top();
+          if (proj.profit >= 0) {
+            w += proj.profit;
+            --k;
+          }
+
+          profitMaxHeap.pop();
+        }
+      }
+    }
+
+    return w;
   }
 };
 
--- c2/main.cpp
+++ c2/main.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "folly/json.h"
 #include "gflags/gflags.h"
+#include "glog/logging.h"
 #include "inc/sept21.h"
 
 DEFINE_int32(lccn_code, 0, "A useless input code");
 
 void runSolution() {
+  std::vector<int> profs{1, 2, 3};
+  std::vector<int> capts{0, 1, 1};
+
   sept21::Solution s{};
-  auto result = s.balancedStringSplit(std::string("LLRRLRLR"));
+  auto result = s.findMaximizedCapital(2, 0, profs, capts);
   std::cout << "Result: " << folly::toPrettyJson(result) << std::endl;
 }
 
 int main(int argc, char **argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+
   std::cout << "Input: Code = " << FLAGS_lccn_code << std::endl;
   std::cout << "========= Solution =========" << std::endl;
   runSolution();
