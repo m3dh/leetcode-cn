1d2db126e4841bbafc01832bb5a1bacff91f73db
Liam Yu <liamyu@live.com>
2021-11-21 10:29:49 -0800

    Sun Nov 21 10:29:49 PST 2021

--- c2/inc/alc.h
+++ c2/inc/alc.h
@@ -1,3 +1,6 @@
+#pragma once
+#include <vector>
+
 namespace alc {
 // Definition for a binary tree node.
 struct TreeNode {
@@ -8,6 +11,21 @@ struct TreeNode {
   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
   TreeNode(int x, TreeNode *left, TreeNode *right)
       : val(x), left(left), right(right) {}
+};
+
+class Node {
+ public:
+  int val;
+  std::vector<Node *> children;
+
+  Node() {}
+
+  Node(int _val) { val = _val; }
+
+  Node(int _val, std::vector<Node *> _children) {
+    val = _val;
+    children = _children;
+  }
 };
 
 }  // namespace alc
--- c2/main.cpp
+++ c2/main.cpp
@@ -9,13 +9,40 @@
 
 DEFINE_int32(lccn_code, 0, "A useless input code");
 
+namespace {
+using namespace std;
+class Solution {
+ public:
+  Solution(vector<int>& nums) : nums{nums} {}
+
+  vector<int> reset() { return nums; }
+
+  vector<int> shuffle() {
+    vector<int> ret{};
+    vector<int> q{nums};
+    ret.reserve(nums.size());
+    for (int i = 0; i < nums.size(); i++) {
+      int idx = std::rand() % q.size();
+      auto iter = q.begin();
+      std::advance(iter, idx);
+      ret.push_back(*iter);
+      q.erase(iter);
+    }
+    return ret;
+  }
+
+ private:
+  vector<int> nums;
+};
+}  // namespace
+
 void runSolution() {
   sept21::Solution s{};
   auto result = s.checkValidString("((*)");
   LOG(INFO) << result << std::endl;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
   FLAGS_alsologtostderr = 1;
