1febca2ad189bcaa78453109223a003db7ef0bad
Liam Yu <liamyu@live.com>
2022-03-13 17:54:56 -0700

    make c2 work

--- c2/inc/alc.h
+++ c2/inc/alc.h
@@ -1,31 +1,35 @@
 #pragma once
 #include <vector>
 
-namespace alc {
-// Definition for a binary tree node.
-struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode() : val(0), left(nullptr), right(nullptr) {}
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right)
-      : val(x), left(left), right(right) {}
-};
+namespace alc
+{
+  // Definition for a binary tree node.
+  struct TreeNode
+  {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+  };
 
-class Node {
- public:
-  int val;
-  std::vector<Node *> children;
+  class Node
+  {
+  public:
+    int val;
+    std::vector<Node *> children;
 
-  Node() {}
+    Node() {}
 
-  Node(int _val) { val = _val; }
+    Node(int _val) { val = _val; }
 
-  Node(int _val, std::vector<Node *> _children) {
-    val = _val;
-    children = _children;
-  }
-};
+    Node(int _val, std::vector<Node *> _children)
+    {
+      val = _val;
+      children = _children;
+    }
+  };
 
-}  // namespace alc
+} // namespace alc
--- c2/main.cpp
+++ c2/main.cpp
@@ -9,40 +9,46 @@
 
 DEFINE_int32(lccn_code, 0, "A useless input code");
 
-namespace {
-using namespace std;
-class Solution {
- public:
-  Solution(vector<int>& nums) : nums{nums} {}
+namespace
+{
+  using namespace std;
+  class Solution
+  {
+  public:
+    Solution(vector<int> &nums) : nums{nums} {}
 
-  vector<int> reset() { return nums; }
+    vector<int> reset() { return nums; }
 
-  vector<int> shuffle() {
-    vector<int> ret{};
-    vector<int> q{nums};
-    ret.reserve(nums.size());
-    for (int i = 0; i < nums.size(); i++) {
-      int idx = std::rand() % q.size();
-      auto iter = q.begin();
-      std::advance(iter, idx);
-      ret.push_back(*iter);
-      q.erase(iter);
+    vector<int> shuffle()
+    {
+      vector<int> ret{};
+      vector<int> q{nums};
+      ret.reserve(nums.size());
+      for (int i = 0; i < nums.size(); i++)
+      {
+        int idx = std::rand() % q.size();
+        auto iter = q.begin();
+        std::advance(iter, idx);
+        ret.push_back(*iter);
+        q.erase(iter);
+      }
+      return ret;
     }
-    return ret;
-  }
 
- private:
-  vector<int> nums;
-};
-}  // namespace
+  private:
+    vector<int> nums;
+  };
+} // namespace
 
-void runSolution() {
+void runSolution()
+{
   sept21::Solution s{};
   auto result = s.checkValidString("((*)");
   LOG(INFO) << result << std::endl;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
   FLAGS_alsologtostderr = 1;
