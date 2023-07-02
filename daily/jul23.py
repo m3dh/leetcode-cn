a4c0b05f3aecb199514268e547c629bd909c33ca
Liam Yu <liamyu@live.com>
2023-07-02 15:58:57 -0700

    jul-02-23

--- daily/jul23.py
+++ daily/jul23.py
@@ -1,4 +1,10 @@
-from typing import List, Tuple
+from typing import List, Tuple, Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
@@ -18,6 +24,46 @@ class Solution:
             else:
                 return [sortedNums[l][0], sortedNums[r][0]]
         return [-1, -1]
+
+    # https://leetcode.cn/problems/add-two-numbers/
+    def addTwoNumbers1(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(val=-1, next=ListNode(val=0))
+        prev = dummy
+        remaining = 0
+        while l1 != None or l2 != None or remaining != 0:
+            v1 = 0
+            v2 = 0
+            if l1 != None:
+                v1 = l1.val
+                l1 = l1.next
+            if l2 != None:
+                v2 = l2.val
+                l2 = l2.next
+            curVal = remaining + v1 + v2
+            remaining = 1 if curVal >= 10 else 0
+            curVal = curVal if curVal < 10 else curVal - 10
+            prev.next = ListNode(val=curVal)
+            prev = prev.next
+        return dummy.next
+
+    # https://leetcode.cn/problems/add-two-numbers-ii/
+    def addTwoNumbers2(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(l: Optional[ListNode]) -> Optional[ListNode]:
+            prev: Optional[ListNode] = None
+            if not l:
+                return l
+            while True:
+                next = l.next
+                l.next = prev
+                prev = l
+                if next:
+                    l = next
+                else:
+                    return l
+        r1 = reverse(l1)
+        r2 = reverse(l2)
+        ret = self.addTwoNumbers1(r1, r2)
+        return reverse(ret)
 
 
 if __name__ == "__main__":
